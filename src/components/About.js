26195b75fc417c415c6977693ac78a2b7429bdfb
Tushar Netale <tusharnetale007@gmail.com>
2025-01-03 20:57:21 +0530

    Updated Home.js to include resume download functionality

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import { motion } from 'framer-motion';
+import React from "react";
+import { motion } from "framer-motion";
 
 const About = () => {
   return (
     <section id="about" className="p-10 bg-gray-100">
       <motion.h2
         className="text-4xl font-bold text-center mb-4"
-        initial={{ x: '-100vw' }}
+        initial={{ x: "-100vw" }}
         animate={{ x: 0 }}
-        transition={{ type: 'spring', stiffness: 50 }}
+        transition={{ type: "spring", stiffness: 50 }}
       >
         About Me
       </motion.h2>
@@ -18,11 +18,13 @@ const About = () => {
         animate={{ opacity: 1 }}
         transition={{ delay: 0.3, duration: 1 }}
       >
-       
-        "Aspiring MCA student with a passion for technology and a solid foundation in Java, cloud 
-        computing and web Development management holding a Bachelor's degree in Computer
-        Applications. Currently pursuing a Master's degree. Passionate about web Development and staying
-        updated on emerging technologies. Committed to innovation and continuous growth."
+        "Motivated software developer with expertise in Java, cloud computing,
+        and web development. Skilled in designing and implementing scalable,
+        efficient software solutions. Excited to work collaboratively with
+        cross-functional teams to refine product architecture, troubleshoot
+        existing systems, and deliver innovative applications. Dedicated to
+        continuous learning and adapting to new technologies for enhancing
+        product performance and usability. "
       </motion.p>
     </section>
   );
